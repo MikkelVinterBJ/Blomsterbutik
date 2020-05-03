d20602f324f786e5eb19380415351fcc30ec0ab1
MikkelVinterBJ <vbjmikkel@gmail.com>
2020-05-03 15:31:03 +0200

    Add project files.

---
+++ Blomsterbutik/BlomstViewModel.cs
@@ -0,0 +1,40 @@
+﻿using BlomstViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blomsterbutik
+{
+    public class BlomstViewModel
+    {
+         public ObservableCollection<OrdreBlomst> Blomster { get; }
+        = new ObservableCollection<OrdreBlomst>();
+
+        public string NavnBlomst { get; set; }
+        public int AntalBlomst { get; set; }
+        public string FarveBlomst {get; set; }
+
+        public RelayCommand AddNyBlomst { get; set; }
+
+        public BlomstViewModel()
+        {
+            
+            Blomster.Add(new OrdreBlomst() { Navn = "Rose", Farve = "Rød", Antal = 4 });
+            Blomster.Add(new OrdreBlomst() { Navn = "Tulipan", Farve = "Gul", Antal = 6 });
+            Blomster.Add(new OrdreBlomst() { Navn = "Rose", Farve = "Lilla", Antal = 3 });
+
+            AddNyBlomst = new RelayCommand(AddBlomst);
+        }
+
+        public void AddBlomst()
+        {
+            OrdreBlomst oBlomst = new OrdreBlomst(NavnBlomst, FarveBlomst, AntalBlomst);
+
+            Blomster.Add(oBlomst);
+        }
+
+    }
+}
---
+++ Blomsterbutik/OrdreBlomst.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blomsterbutik
+{
+    public class OrdreBlomst
+    {
+		private int _antal;
+		private string _navn;
+		private string _farve;
+
+		public string Farve
+		{
+			get { return _farve; }
+			set { _farve = value; }
+		}
+
+
+		public int Antal
+		{
+			get { return _antal; }
+			set { _antal = value; }
+		}
+
+
+		public string Navn
+		{
+			get { return _navn; }
+			set { _navn = value; }
+		}
+
+		public OrdreBlomst(string navn, string farve, int antal)
+		{
+			Navn = navn;
+			Antal = antal;
+			Farve = farve;
+
+
+
+
+		}
+
+		public OrdreBlomst()
+		{
+
+		}
+
+	}
+}
