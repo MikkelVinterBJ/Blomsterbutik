f831f8d0951a06e239915a77d833433e6a4b794b
MikkelVinterBJ <vbjmikkel@gmail.com>
2020-05-13 13:29:36 +0200

    Json opdatering

--- Blomsterbutik/BlomstViewModel.cs
+++ Blomsterbutik/BlomstViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using BlomstViewModel;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace Blomsterbutik
 {
@@ -17,9 +20,19 @@ namespace Blomsterbutik
         public int AntalBlomst { get; set; }
         public string FarveBlomst {get; set; }
 
+
+
+        public string Jsonblomster { get; set; }
+
+        StorageFolder localfolder = null;
+
+        private readonly string filnavn = "blomster.json";
+
         public RelayCommand AddNyBlomst { get; set; }
         public OrdreBlomst SelectedOrdreBlomst { get; set; }
         public RelayCommand SletSelectedBlomst { get; set; }
+        public RelayCommand GemData { get; set; }
+        public RelayCommand HentData  { get; set; }
 
 
         public BlomstViewModel()
@@ -31,7 +44,11 @@ namespace Blomsterbutik
 
             AddNyBlomst = new RelayCommand(AddBlomst);
             SelectedOrdreBlomst = new OrdreBlomst();
-            SletSelectedBlomst = new RelayCommand(SletBlomst);
+            SletSelectedBlomst = new RelayCommand(SletBlomst, canDeleteBlomsterListe);
+            GemData = new RelayCommand(GemDataTilDiskAsync);
+            HentData = new RelayCommand(HentDataFraDiskAsync);
+
+            localfolder = ApplicationData.Current.LocalFolder;
 
         }
 
@@ -40,11 +57,59 @@ namespace Blomsterbutik
             OrdreBlomst oBlomst = new OrdreBlomst(NavnBlomst, FarveBlomst, AntalBlomst);
 
             Blomster.Add(oBlomst);
+
+            SletSelectedBlomst.RaiseCanExecuteChanged();
         }
 
         public void SletBlomst()
         {
             Blomster.Remove(SelectedOrdreBlomst);
+            SletSelectedBlomst.RaiseCanExecuteChanged();
         }
+
+        private bool canDeleteBlomsterListe()
+        {
+            return Blomster.Count > 0;
+        }
+
+        private string GetJson()
+        {
+            string json = JsonConvert.SerializeObject(Blomster);
+            return json;
+
+        }
+
+        private void IndsætJson(string jsonText)
+        {
+            List<OrdreBlomst> nyliste = JsonConvert.DeserializeObject<List<OrdreBlomst>>(jsonText);
+                
+            foreach (var blomst in nyliste)
+            {
+                this.Blomster.Add(blomst);
+            }
+            SletSelectedBlomst.RaiseCanExecuteChanged();
+        }
+
+        private async void GemDataTilDiskAsync()
+        {
+
+            this.Jsonblomster = GetJson();
+            StorageFile file = await localfolder.CreateFileAsync(filnavn,CreationCollisionOption.ReplaceExisting);
+
+            await FileIO.WriteTextAsync(file, this.Jsonblomster);
+
+        }
+
+        private async void HentDataFraDiskAsync()
+        {
+            StorageFile file = await localfolder.GetFileAsync(filnavn);
+            string jsontext = await FileIO.ReadTextAsync(file);
+            this.Blomster.Clear();
+            IndsætJson(jsontext);
+
+            SletSelectedBlomst.RaiseCanExecuteChanged();
+
+        }
+
     }
 }
--- Blomsterbutik/OrdreBlomst.cs
+++ Blomsterbutik/OrdreBlomst.cs
@@ -1,38 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Blomsterbutik
+namespace Blomsterbutik 
 {
-    public class OrdreBlomst
-    {
+    public class OrdreBlomst : INotifyPropertyChanged
+	{
 		private int _antal;
 		private string _navn;
 		private string _farve;
 
+		public event PropertyChangedEventHandler PropertyChanged;
+
 		public string Farve
 		{
 			get { return _farve; }
-			set { _farve = value; }
+			set { _farve = value;
+				OnPropertyChanged();
+			}
+
 		}
 
 
 		public int Antal
 		{
 			get { return _antal; }
-			set { _antal = value; }
+			set { _antal = value;
+				OnPropertyChanged();
+			}
 		}
 
 
 		public string Navn
 		{
 			get { return _navn; }
-			set { _navn = value; }
+			set { _navn = value;
+				OnPropertyChanged();
+			}
 		}
 
-		public OrdreBlomst(string navn, string farve, int antal)
+		public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+
+			public OrdreBlomst(string navn, string farve, int antal)
 		{
 			Navn = navn;
 			Antal = antal;
